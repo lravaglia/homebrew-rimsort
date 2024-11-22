76842846b434e4e4948650a11c8412e3e8fb6fc7
Lorenzo Ravaglia <lorenzo@ravaglia.xyz>
2024-11-22 21:54:05 +0000

    fix: correct download path

--- Casks/rimsort.rb
+++ Casks/rimsort.rb
@@ -1,8 +1,8 @@
 cask "rimsort" do
-    version "v1.0.10"
+    version "1.0.10"
 #   sha256 "c87dbc6aff5411676a471e84905d69c671b62b93b1210bd95c9d776d087de95c"
 
-    url "https://github.com/RimSort/RimSort/releases/download/#{version}/RimSort-#{version}.zip"
+    url "https://github.com/RimSort/RimSort/releases/download/v#{version}/RimSort-v#{version}-Darwin_#{arch}.zip"
     name "RimSort"
     desc "Rimworld modlist manager"
     homepage "https://rimsort.github.io/RimSort/"
